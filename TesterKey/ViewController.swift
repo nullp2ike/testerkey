7ba57183b4a5b9ea2a673da78eac0873f24abac3
Risko Ruus <risko.ruus@gmail.com>
2016-07-19 21:35:53 +0300

    Added settings button and text field to host app to help testing

--- TesterKey/ViewController.swift
+++ TesterKey/ViewController.swift
@@ -13,10 +13,28 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        var defaults = NSUserDefaults(suiteName: "group.eu.testandrest.1000Fingers")
+        let defaults = NSUserDefaults(suiteName: "group.eu.testandrest.1000Fingers")
         defaults?.setObject("It worked!", forKey: "alarmTime")
         defaults?.synchronize()
+        
+        let settingsButton = UIButton(frame: CGRect(x: 100, y: 100, width: 200, height: 50))
+        settingsButton.backgroundColor = .greenColor()
+        settingsButton.setTitle("Keyboard Settings", forState: .Normal)
+        settingsButton.addTarget(self, action: #selector(buttonAction), forControlEvents: .TouchUpInside)
+        
+        let textField = UITextField(frame: CGRect(x: 100, y: 200, width: 200, height: 50))
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = UIColor.blackColor().CGColor
+        
+        self.view.addSubview(settingsButton)
+        self.view.addSubview(textField)
     }
+    
+    func buttonAction(sender: UIButton!) {
+        UIApplication.sharedApplication().openURL(NSURL(string:"prefs:root=General&path=Keyboard")!)
+        print("Keyboard settings opened")
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
